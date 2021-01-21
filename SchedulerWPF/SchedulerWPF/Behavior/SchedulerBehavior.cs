0911573c20d53e8c1de3a7211d7ed5975da77759
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-21 16:43:21 +0530

    WPF-56592 - Sample and screenshot added.

---
+++ SchedulerWPF/SchedulerWPF/Behavior/SchedulerBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Xaml.Behaviors;
+using Syncfusion.UI.Xaml.Scheduler;
+
+namespace WpfScheduler
+{
+    public class SchedulerBehavior : Behavior<SfScheduler>
+    {
+        SfScheduler scheduler;
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            scheduler = this.AssociatedObject;
+            this.AssociatedObject.DisplayDate = DateTime.Now.Date.AddHours(11);
+            this.AssociatedObject.MinimumDate = DateTime.Now.Date;
+            this.AssociatedObject.MaximumDate = DateTime.Now.Date;
+        }        
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.scheduler = null;
+        }
+    }
+}
